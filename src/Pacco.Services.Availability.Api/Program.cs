9b341917282f5f5e20a377258d43e17449600ff0
Thiago Cruz <thagocruz@gmail.com>
2021-10-29 12:12:45 -0300

    First commit

---
+++ src/Pacco.Services.Availability.Api/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Pacco.Services.Availability.Api
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+            => await CreateWebHostBuilder(args)
+                .Build()
+                .RunAsync();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+            => WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
