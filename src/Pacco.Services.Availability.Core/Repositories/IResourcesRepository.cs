6999c305392d5993d88169ab063e8f9eca77a700
Thiago Cruz <thagocruz@gmail.com>
2021-10-29 16:58:25 -0300

    Create IResourcesRepository.cs

---
+++ src/Pacco.Services.Availability.Core/Repositories/IResourcesRepository.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Pacco.Services.Availability.Core.Entities;
+
+namespace Pacco.Services.Availability.Core.Repositories
+{
+    public interface IResourcesRepository
+    {
+        Task<Resource> GetAsync(AggregateId id);
+        Task AddAsync(Resource resource);
+        Task UpdateAsync(Resource resource);
+        Task DeleteAsync(AggregateId id);
+    }
+}
