441e181d5dffe5b110ef545467876725eb1118e9
Thiago Cruz <thagocruz@gmail.com>
2021-10-29 15:28:37 -0300

    Update AggregateId.cs

--- src/Pacco.Services.Availability.Core/Entities/AggregateId.cs
+++ src/Pacco.Services.Availability.Core/Entities/AggregateId.cs
@@ -1,15 +1,14 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using Pacco.Services.Availability.Core.Exceptions;
 
 namespace Pacco.Services.Availability.Core.Entities
 {
-    public class AggregateId
+    public class AggregateId : IEquatable<AggregateId>
     {
         public Guid Value { get; }
 
-        public AggregateId() : this(Guid.NewGuid())
-        {
-            
-        }
+        public AggregateId() : this(Guid.NewGuid()) { }
 
         public AggregateId(Guid value)
         {
@@ -17,6 +16,25 @@ namespace Pacco.Services.Availability.Core.Entities
             {
                 throw new InvalidAggregateIdException(value);
             }
+
+            Value = value;
         }
+
+        public bool Equals([AllowNull] AggregateId other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Value.Equals(other.Value);
+        }
+
+        public override bool Equals([AllowNull] object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if(obj.GetType() != this.GetType()) return false;
+            return Value.Equals((AggregateId) obj);
+        }
+
+        public override int GetHashCode() => Value.GetHashCode();
     }
 }
