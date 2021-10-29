b706b89e375485896b259010eaab03e7afac496d
Thiago Cruz <thagocruz@gmail.com>
2021-10-29 15:38:27 -0300

    Update AggregateRoot.cs

--- src/Pacco.Services.Availability.Core/Entities/AggregateRoot.cs
+++ src/Pacco.Services.Availability.Core/Entities/AggregateRoot.cs
@@ -1,8 +1,18 @@
+using System.Collections.Generic;
+using Pacco.Services.Availability.Core.Events;
+
 namespace Pacco.Services.Availability.Core.Entities
 {
-    public class AggregateRoot
+    public abstract class AggregateRoot
     {
+        private readonly List<IDomainEvent> _events = new List<IDomainEvent>();
         public AggregateId Id { get; protected set; }
         public int Version { get; protected set; }
+        public IEnumerable<IDomainEvent> Events => _events;
+
+        protected void AddEvent(IDomainEvent @event)
+        {
+
+        }
     }
 }
