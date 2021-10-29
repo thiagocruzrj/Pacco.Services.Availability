2a16a18848cc39ef0bf3069ccef293cca2af9519
Thiago Cruz <thagocruz@gmail.com>
2021-10-29 16:18:27 -0300

    Create Reservation.cs

---
+++ src/Pacco.Services.Availability.Core/ValueObjects/Reservation.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Pacco.Services.Availability.Core.ValueObjects
+{
+    public struct Reservation : IEquatable<Reservation>
+    {
+        public DateTime DateTime { get; }
+        public int Priority { get; }
+
+        public Reservation(DateTime dateTime, int priority) => (DateTime, Priority) = (dateTime, priority);
+
+        public bool Equals(Reservation other) => DateTime.Equals(other.DateTime) && Priority == other.Priority;
+
+        public override bool Equals(object obj) => obj is Reservation other && Equals(other);
+
+        public override int GetHashCode() => HashCode.Combine(DateTime, Priority);
+    }
+}
