9c585aaa2a631fa2f02f5199e0d1833ba3d6f626
Thiago Cruz <thagocruz@gmail.com>
2021-11-01 18:31:59 -0300

    Update ResourcesMongoRepository.cs

--- src/Pacco.Services.Availability.Infrastructure/Mongo/Repositories/ResourcesMongoRepository.cs
+++ src/Pacco.Services.Availability.Infrastructure/Mongo/Repositories/ResourcesMongoRepository.cs
@@ -1,11 +1,16 @@
 using System.Threading.Tasks;
 using Pacco.Services.Availability.Core.Entities;
 using Pacco.Services.Availability.Core.Repositories;
+using Convey.Persistence.MongoDB;
 
 namespace Pacco.Services.Availability.Infrastructure.Mongo.Repositories
 {
     internal sealed class ResourcesMongoRepository : IResourcesRepository
     {
+        public ResourcesMongoRepository(IMongoRepository<Resource> repo)
+        {
+            
+        }
         public Task AddAsync(Resource resource)
         {
             throw new System.NotImplementedException();
