1d1ad2beea4ac4739ca4ba3c1a27d2f7fee6bd61
Thiago Cruz <thagocruz@gmail.com>
2021-11-01 18:03:44 -0300

    Update AddResourceHandler.cs

--- src/Pacco.Services.Availability.Application/Commands/Handlers/AddResourceHandler.cs
+++ src/Pacco.Services.Availability.Application/Commands/Handlers/AddResourceHandler.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Convey.CQRS.Commands;
+using Pacco.Services.Availability.Core.Entities;
 using Pacco.Services.Availability.Core.Repositories;
 
 namespace Pacco.Services.Availability.Application.Commands.Handlers
@@ -21,6 +22,9 @@ namespace Pacco.Services.Availability.Application.Commands.Handlers
             {
                 throw new ResourceAlreadyExistException(command.ResourceId);
             }
+
+            resource = Resource.Create(command.ResourceId, command.Tags);
+            await _resourcesRepository.AddAsync(resource);
         }
     }
 }
