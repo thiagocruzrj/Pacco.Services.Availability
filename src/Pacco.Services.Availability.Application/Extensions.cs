e20abcf3e1f1dde6b5a9d4e9bfa1580bafcfb514
Thiago Cruz <thagocruz@gmail.com>
2021-11-01 18:21:36 -0300

    Create Extensions.cs

---
+++ src/Pacco.Services.Availability.Application/Extensions.cs
@@ -0,0 +1,15 @@
+using Convey;
+using Convey.CQRS.Commands;
+using Convey.CQRS.Queries;
+
+namespace Pacco.Services.Availability.Application
+{
+    public static class Extensions
+    {
+        public static IConveyBuilder AddApplication(this IConveyBuilder builder) =>
+            builder.AddCommandHandlers()
+                   .AddQueryHandlers()
+                   .AddInMemoryCommandDispatcher()
+                   .AddInMemoryQueryDispatcher();
+    }
+}
