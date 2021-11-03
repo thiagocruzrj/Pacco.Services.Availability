62a58120472d9869c8d4c890a1ac3ebca38164ca
Thiago Cruz <thagocruz@gmail.com>
2021-11-03 16:30:29 -0300

    Create ResourceDto.cs

---
+++ src/Pacco.Services.Availability.Application/DTO/ResourceDto.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Pacco.Services.Availability.Application.DTO
+{
+    public class ResourceDto
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+    }
+}
