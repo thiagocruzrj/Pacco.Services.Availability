529de21fa4d2db02675e908e6f814151f0e18f14
Thiago Cruz <thagocruz@gmail.com>
2021-11-03 11:49:42 -0300

    Update Extensions.cs

--- src/Pacco.Services.Availability.Infrastructure/Mongo/Documents/Extensions.cs
+++ src/Pacco.Services.Availability.Infrastructure/Mongo/Documents/Extensions.cs
@@ -11,6 +11,19 @@ namespace Pacco.Services.Availability.Infrastructure.Mongo.Documents
             new Resource(document.Id, document.Tags, document.Reservations?
                 .Select(r => new Reservation(r.TimeStamp.AsDateTime(), r.Priority)));
 
+        public static ResourceDocument AddDocument(this Resource entity) =>
+            new ResourceDocument
+            {
+                Id = entity.Id,
+                Version = entity.Version,
+                Tags = entity.Tags,
+                Reservations = entity.Reservations.Select(r => new ReservationDocument
+                {
+                    TimeStamp = r.DateTime.AsDaysSinceEpoch(),
+                    Priority = r.Priority
+                })
+            };
+
         public static int AsDaysSinceEpoch(this DateTime dateTime) =>
             (dateTime - new DateTime()).Days;
 
