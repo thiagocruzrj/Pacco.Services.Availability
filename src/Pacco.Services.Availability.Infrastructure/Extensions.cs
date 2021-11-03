f965230452465aa4596dc1956c96ca09a2c29f86
Thiago Cruz <thagocruz@gmail.com>
2021-11-03 12:38:37 -0300

    Update Extensions.cs

--- src/Pacco.Services.Availability.Infrastructure/Extensions.cs
+++ src/Pacco.Services.Availability.Infrastructure/Extensions.cs
@@ -1,7 +1,11 @@
 using System;
 using Convey;
 using Convey.Persistence.MongoDB;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Pacco.Services.Availability.Core.Repositories;
 using Pacco.Services.Availability.Infrastructure.Mongo.Documents;
+using Pacco.Services.Availability.Infrastructure.Mongo.Repositories;
 
 namespace Pacco.Services.Availability.Infrastructure
 {
@@ -9,9 +13,19 @@ namespace Pacco.Services.Availability.Infrastructure
     {
         public static IConveyBuilder AddInfrastructure(this IConveyBuilder builder)
         {
-            builder.AddMongo().AddMongoRepository<ResourceDocument, Guid>("resources");
+            builder.Services.AddTransient<IResourcesRepository, ResourcesMongoRepository>();
+            
+            builder
+                .AddMongo()
+                .AddMongoRepository<ResourceDocument, Guid>("resources");
 
             return builder;
+        }
+
+        public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+        {
+            app.UseConvey();
+            return app;
         }
     }
 }
