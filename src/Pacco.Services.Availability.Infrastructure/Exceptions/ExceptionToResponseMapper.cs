abc28f7af396cf179e052d3b5f251819df713e36
Thiago Cruz <thagocruz@gmail.com>
2021-12-08 17:55:24 -0300

    Implement getCode method midware

--- src/Pacco.Services.Availability.Infrastructure/Exceptions/ExceptionToResponseMapper.cs
+++ src/Pacco.Services.Availability.Infrastructure/Exceptions/ExceptionToResponseMapper.cs
@@ -1,13 +1,45 @@
 using System;
+using System.Collections.Concurrent;
+using System.Net;
+using Convey;
 using Convey.WebApi.Exceptions;
+using Pacco.Services.Availability.Application.Commands.Handlers;
+using Pacco.Services.Availability.Core.Exceptions;
 
 namespace Pacco.Services.Availability.Infrastructure.Exceptions
 {
     internal sealed class ExceptionToResponseMapper : IExceptionToResponseMapper
     {
-        public ExceptionResponse Map(Exception exception)
+        private static readonly ConcurrentDictionary<Type, string> Codes = new ConcurrentDictionary<Type, string>();
+        public ExceptionResponse Map(Exception exception) =>
+            exception switch
+            {
+                DomainException ex => new ExceptionResponse(new {code = GetCode(ex), reason = ex.Message}, 
+                    HttpStatusCode.BadRequest),
+                AppException ex => new ExceptionResponse(new {code = GetCode(ex), reason = ex.Message}, 
+                    HttpStatusCode.BadRequest),
+                _ => new ExceptionResponse(new {code = "error", reason = "There was an error."}, 
+                    HttpStatusCode.InternalServerError)
+            };
+
+        private static string GetCode(Exception exception)
         {
-            throw new NotImplementedException();
+            var type = exception.GetType();
+            if(Codes.TryGetValue(type, out var code))
+            {
+                return code;
+            }
+
+            var exceptionCode = exception switch
+            {
+                DomainException ex when !string.IsNullOrWhiteSpace(ex.Code) => ex.Code,
+                AppException ex when !string.IsNullOrWhiteSpace(ex.Code) => ex.Code,
+                _ => type.Name.Underscore().Replace("_exception", string.Empty)
+            };
+
+            Codes.TryAdd(type, exceptionCode);
+
+            return exceptionCode;
         }
     }
 }
