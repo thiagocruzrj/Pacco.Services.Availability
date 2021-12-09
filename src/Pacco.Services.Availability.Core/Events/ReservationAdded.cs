d75b233d6c465415adf34f648a745edf77febce2
Thiago Cruz <thagocruz@gmail.com>
2021-12-09 11:40:26 -0300

    Create ReservationAdded.cs

---
+++ src/Pacco.Services.Availability.Core/Events/ReservationAdded.cs
@@ -0,0 +1,18 @@
+using Pacco.Services.Availability.Core.Entities;
+using Pacco.Services.Availability.Core.ValueObjects;
+
+namespace Pacco.Services.Availability.Core.Events
+{
+    public class ReservationAdded : IDomainEvent
+    {
+        public Resource Resource { get; }
+        public Reservation Reservation { get; set; }
+
+        public ReservationAdded(Resource resource, Reservation reservation)
+        {
+            Resource = resource;
+            Reservation = reservation;
+
+        }
+    }
+}
