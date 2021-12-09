a1ce87bee540d0623f1f4934ba53af31961a9f83
Thiago Cruz <thagocruz@gmail.com>
2021-12-09 12:03:53 -0300

    Update ResourceAlreadyExistException.cs

--- src/Pacco.Services.Availability.Application/AppExceptions/ResourceAlreadyExistException.cs
+++ src/Pacco.Services.Availability.Application/AppExceptions/ResourceAlreadyExistException.cs
@@ -4,7 +4,6 @@ namespace Pacco.Services.Availability.Application.Commands.Handlers
 {
     public class ResourceAlreadyExistException : AppException
     {
-
         public ResourceAlreadyExistException(Guid id) : base($"Resource with ID: '{id}' already exists.") { }
     }
 }
