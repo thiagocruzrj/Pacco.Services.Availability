d3ab54469aca6023508dae59bf48f5d3775bf6a3
Thiago Cruz <thagocruz@gmail.com>
2021-12-09 11:39:41 -0300

    Create ReservationCanceled.cs

---
+++ src/Pacco.Services.Availability.Core/Events/ReservationCanceled.cs
@@ -0,0 +1,18 @@
+using Pacco.Services.Availability.Core.Entities;
+using Pacco.Services.Availability.Core.ValueObjects;
+
+namespace Pacco.Services.Availability.Core.Events
+{
+    public class ReservationCanceled : IDomainEvent
+    {
+        public Resource Resource { get; }
+        public Reservation Reservation { get; set; }
+
+        public ReservationCanceled(Resource resource, Reservation reservation)
+        {
+            Resource = resource;
+            Reservation = reservation;
+
+        }
+    }
+}
